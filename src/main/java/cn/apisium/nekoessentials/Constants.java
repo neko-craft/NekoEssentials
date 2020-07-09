04171db34953d0a2be0872c4b5054bdcd5ac3929
Shirasawa <764798966@qq.com>
2020-07-09 16:15:04 +0800

    Update

--- src/main/java/cn/apisium/nekoessentials/Constants.java
+++ src/main/java/cn/apisium/nekoessentials/Constants.java
@@ -11,20 +11,20 @@ public final class Constants {
     public static final String NO_PERMISSION = "§c你没有权限来执行这个指令!";
     public static final String NO_SUCH_PLAYER = "§c没有这样的一个玩家!";
 
-    public static final String MESSAGE_HEADER = "§e§m                    §r §e[传送系统] §b§m                    ";
+    public static final String MESSAGE_HEADER = "§b§m                    §r §e[传送系统] §b§m                    ";
     public static final String MESSAGE_FOOTER = "§b§m                                                       ";
     public static final BaseComponent[] CANCEL_HUB = new BaseComponent[3];
     public static final BaseComponent[] REQUEST_HUB = new BaseComponent[4];
 
     static {
-        CANCEL_HUB[0] = new TextComponent("  §d正在传送中... 传送中禁止移动, 如需取消请点击下面的文字:\n");
-        CANCEL_HUB[1] = new TextComponent("        ");
+        CANCEL_HUB[0] = new TextComponent("  §d正在传送中... §7传送中禁止移动, 如需取消请点击下面的文字:\n");
+        CANCEL_HUB[1] = new TextComponent("                  ");
         TextComponent t = new TextComponent("[取消传送]");
         t.setColor(ChatColor.RED);
         t.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/tpcancel"));
         CANCEL_HUB[2] = t;
 
-        REQUEST_HUB[0] = new TextComponent("        ");
+        REQUEST_HUB[0] = new TextComponent("            ");
         t = new TextComponent("[拒绝传送]");
         t.setColor(ChatColor.RED);
         t.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/tpdeny"));
