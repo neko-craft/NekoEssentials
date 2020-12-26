f202604ea4cc01746d3ec430f6d51332f9c1586a
BBleae <bbleae@baka.studio>
2020-12-27 00:11:13 +0800

    fix: spectators can set home

--- src/main/java/cn/apisium/nekoessentials/commands/SetHomeCommand.java
+++ src/main/java/cn/apisium/nekoessentials/commands/SetHomeCommand.java
@@ -3,6 +3,7 @@ package cn.apisium.nekoessentials.commands;
 import cn.apisium.nekoessentials.Main;
 import cn.apisium.nekoessentials.utils.DatabaseSingleton;
 import cn.apisium.nekoessentials.utils.Serializer;
+import org.bukkit.GameMode;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
@@ -16,6 +17,10 @@ public final class SetHomeCommand extends BasicCommand {
     public boolean callback(CommandSender sender, String[] args) {
         if (!(sender instanceof Player)) return false;
         final Player p = (Player) sender;
+        if (p.getGameMode() == GameMode.SPECTATOR) {
+            p.sendMessage("§c你不能在旁观模式设置家!");
+            return false;
+        }
         DatabaseSingleton.INSTANCE.setPlayerData(p, "home", Serializer.serializeLocation(p.getLocation()));
         p.sendMessage("§a成功设置家!");
         return true;
